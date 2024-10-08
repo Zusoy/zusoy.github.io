63ba7a3ac9c4123f5efbeeb021ae48ae6d5e5fc9
Zusoy <gregoire.drapeau79@gmail.com>
2024-10-09 00:06:07 +0200

    Added resume window

--- src/features/Window/Resume.tsx
+++ src/features/Window/Resume.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Window, { type TaskProps } from 'features/Window'
-import Button from 'widgets/Form/Button'
 
 const Resume: React.FC<TaskProps> = ({ id, index, icon, status }) => {
   return (
     <Window index={index} id={id} title='Resume' icon={icon} status={status}>
-      <h1>Resume</h1>
-      <Button label='Hello' onClick={() => {}} icon='/assets/icons/file.png' />
+      <div className='flex flex-col flex-grow gap-6 w-full h-full p-2 justify-start'>
+        <embed type='application/pdf' src='/assets/resume.pdf' className='w-full h-full'></embed>
+      </div>
     </Window>
   )
 }
