f33d2c24d701bdbdb4c96e9305f5ccc9df3a2314
Zusoy <gregoire.drapeau79@gmail.com>
2024-10-08 12:50:00 +0200

    Adding widgets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import tailwind from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsConfigPaths(), react()],
+  css: {
+    postcss: {
+      plugins: [tailwind()]
+    }
+  }
 })
