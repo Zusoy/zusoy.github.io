60dd60aa2a332d850737f1ebba61b11dc1599a8f
Grégoire Drapeau <gregoire.drapeau79@gmail.com>
2024-10-08 11:40:25 +0200

    Added timer static for yet (#2)

--- src/features/StartMenu/index.tsx
+++ src/features/StartMenu/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Menu from 'features/StartMenu/Menu'
 import StartButton from 'features/StartMenu/StartButton'
+import Timer from 'features/StartMenu/Timer'
 
 const StartMenu: React.FC = () => {
   const [opened, setOpened] = React.useState<boolean>(false)
@@ -9,6 +10,8 @@ const StartMenu: React.FC = () => {
     <nav className='flex items-center absolute bottom-0 w-full min-w-full bg-win-95-silver border-solid border-t-2 border-[white] min-h-10 max-h-10'>
       <Menu opened={opened} />
       <StartButton onClick={() => setOpened(o => !o)}/>
+      <div className='flex-grow'></div>
+      <Timer />
     </nav>
   )
 }
